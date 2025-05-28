3765be935f354acf316c296b46aa4d2317718b4f
kaiyoria <putricylo@hotmail.com>
2025-05-29 01:26:40 +0800

    Create app.py

---
+++ Code/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from dotenv import load_dotenv
+import os
+from groq_openapi import mychatbot
+load_dotenv()
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_msg = event.message.text
+    groq_response = mychatbot(user_msg)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=groq_response)
+    )
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
