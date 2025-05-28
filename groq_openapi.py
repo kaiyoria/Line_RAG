ec3760e906ee0a8c71382a61c600dffcec46ce44
kaiyoria <putricylo@hotmail.com>
2025-05-28 09:47:54 +0800

    Add files via upload

---
+++ groq_openapi.py
@@ -0,0 +1,26 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()  # 載入.env環境變數檔
+
+model = "llama3-70b-8192"
+client = OpenAI(
+    api_key=os.getenv('GROQ_API_KEY'),
+    base_url="https://api.groq.com/openai/v1"
+)
+
+messages = [{"role": "system", "content": "請用中文回覆我，你是有幫助的LINE助手"}]
+
+def mychatbot(prompt):
+    messages.append({"role": "user", "content": prompt})
+    chat_completion = client.chat.completions.create(
+        messages=messages,
+        model=model,
+    )
+    reply = chat_completion.choices[0].message.content
+    messages.append({"role": "assistant", "content": reply})
+    return reply
+
+if __name__ == "__main__":
+    print(mychatbot('我想摸大白熊 ?'))
